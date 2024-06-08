dd628cd7783b98aa5663501c2637d49ae56f2442
John <patrick.meyerin@gmail.com>
2024-06-08 18:49:39 -0500

    first protocol-sn update

--- src/app/project-link/project-link.component.ts
+++ src/app/project-link/project-link.component.ts
@@ -5,14 +5,14 @@ import {Component, Input} from '@angular/core';
   template: `
     <button mat-button [matMenuTriggerFor]="menu">{{projectName}}</button>
     <mat-menu #menu="matMenu">
-      <a style="text-decoration: none" target="_blank" href='{{projectLink}}'><button mat-menu-item>Live project</button></a>
+      <a *ngIf="projectLink" style="text-decoration: none" target="_blank" href='{{projectLink}}'><button mat-menu-item>Live project</button></a>
       <a style="text-decoration: none" target="_blank" href='{{projectSrcLink}}'><button mat-menu-item>Source code</button></a>
     </mat-menu>
   `,
   styleUrls: ['./project-link.component.css']
 })
 export class ProjectLinkComponent {
-  @Input("projectName") projectName = "Project name";
-  @Input("projectLink") projectLink = "Project link";
-  @Input("projectSrcLink") projectSrcLink = "Project source link";
+  @Input("projectName") projectName = "";
+  @Input("projectLink") projectLink = "";
+  @Input("projectSrcLink") projectSrcLink = "";
 }
